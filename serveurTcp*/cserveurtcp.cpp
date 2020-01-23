5859d7bf514a6b186e2c3dee51cbb706d9e43b78
SCHOLL Léo <sti2dlab.schollleo@gmail.com>
2020-01-23 16:17:07 +0100

    initial commit

---
+++ serveurTcp*/cserveurtcp.cpp
@@ -0,0 +1,82 @@
+#include "cserveurtcp.h"
+
+CServeurTcp::CServeurTcp(QObject *parent) :
+    QTcpServer(parent)
+{
+    m_noport = PORTPARDEFAUT;
+    init();
+}
+
+CServeurTcp::CServeurTcp(QObject *parent, quint16 noPort) :
+    QTcpServer(parent)
+{
+    m_noport = noPort;
+    init();
+}
+
+CServeurTcp::~CServeurTcp()
+{
+    for (int i=0 ; i<listeClients.size() ; i++) {
+        listeClients.at(i)->close();
+        delete listeClients.at(i);
+    }
+}
+
+int CServeurTcp::emettreVersClient(QString mess)
+{
+    for (int i=0 ; i<listeClients.size() ; i++) {
+        listeClients.at(i)->write(mess.toStdString().c_str());
+        qDebug() << "Envoi vers " << listeClients.at(i);
+    }
+    return 1;
+}
+
+int CServeurTcp::init()
+{
+    listen(QHostAddress::Any, m_noPort);
+    connect(this,SIGNAL(newConnection()), this, SLOT(onNewConnectionClient()));
+    connect(this, SIGNAL(acceptError(QAbstractSocket::SocketError)), this, SLOT(onErreurReseau(QAbstractSocket::SocketError)));
+    return1;
+}
+
+void CServeurTcp::OnDisconnectedClient()
+{
+    QString mess="Un client vient de se connecter";
+    qDebug() << mess;
+    QTcpSocket *newClient = this->nextPendingConnection();
+    qDebug() << "Nouvelle connection : " << newClient;
+    if (newClient == NULL)
+        emit sigErreur(QAbstractSocket::ConnectionRefusedError);
+    connect(newClient, SIGNAL(readyRead()), this, SLOT(onReadyReadClient()));
+    connect(newClient, SIGNAL(disconnected()), this, SLOT(OnDisconnectedClient()));
+    listeClients.append(newClient);
+    emit sigEvenementServeur("CON");
+    emit sigAdrClient(newClient->localAddress().toString());
+    emit sigMajClients(listeClients);
+}
+
+void CServeurTcp::OnDisconnectedClient()
+{
+    QTcpSocket *client = (QTcpSocket *)sender();
+    emit sigEvenementServeur("DEC");
+    listeClients.removeOne(client);
+    delete client;
+    emit sigMajClients(listeClients);
+}
+
+void CServeurTcp::onErreurReseau(QAbstract::SocketError err)
+{
+    qDebug() << "erreur réseau !";
+    emit sigErreur(err);
+}
+
+void CServeurTcp::onReadyReadClient()
+{
+    QByteArray ba;
+    QTcpSocket *client = (QTcpSocket *)sender();
+    ba=client->readAll();
+    qDebug() << "Client : " << client << ba.size() << " Caracteres reçus";
+    emit sigDataClient(client->localAddress().toString(), QString(ba));
+}
+
+

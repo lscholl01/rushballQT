be75183044dec73f6336778558d47885880453a9
SCHOLL Léo <sti2dlab.schollleo@gmail.com>
2020-01-24 09:01:27 +0100

    first build success

---
+++ serveurTcp/cserveurtcp.h
@@ -0,0 +1,43 @@
+#ifndef CSERVEURTCP_H
+#define CSERVEURTCP_H
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+
+#define PORTPARDEFAUT 2222
+
+class CServeurTcp : public QTcpServer
+{
+    Q_OBJECT
+public:
+    explicit CServeurTcp(QObject *parent = 0);
+    explicit CServeurTcp(QObject *parent = 0, quint16 noPort = 2222);
+    ~CServeurTcp();
+    int emettreVersClient(QString mess);
+
+signals:
+    void sigEvenementServeur(QString eve);
+    void sigErreur(QAbstractSocket::SocketError err);
+    void sigDataClient(QString adrIpClient, QString data);
+    void sigAdrClient(QString adrClient);
+    void sigMajClients(QList<QTcpSocket *> liste);
+
+public slots:
+    void onNewConnectionClient();
+    void OnDisconnectedClient();
+    void onErreurReseau(QAbstractSocket::SocketError err);
+    void onReadyReadClient();
+
+private:
+    int init();
+    quint16 m_noport;
+    QList<QTcpSocket *> listeClients;
+};
+
+
+
+
+
+
+#endif // CSERVEURTCP_H
